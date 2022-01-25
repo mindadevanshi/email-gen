d1c2033cb27f924e1570d63e42580982bb57c5d6
mindadevanshi <84984462+mindadevanshi@users.noreply.github.com>
2022-01-25 19:56:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,26 @@ class User(db.Model):
     email = db.Column(db.String(50), unique=True, primary_key=True)
     password = db.Column(db.String(256), unique=True)
     
+    def __init__(self, name, username, email, password):
+        self.firstname = firstname.title()
+        self.lastname = lastname.title()
+        self.email = email.lower()
+        self.set_password(password)
+    
+    def set_password(self, password):
+        self.pwdhash = (password)
+
+    def check_password(self, password):
+        return password
+    
+    
+    db.create_all()
+    db.session.commit()
+    admin = User('admi2', 'admin@ex1ample.com', 'admin', 'admin2@example.com')
+    db.session.add(admin)
+    db.session.commit()
+
+    
     
 @app.route('/')
 def dashboard():
